7cd2c104dbd779acf02133ae81e57362269351be
codeweb123 <jiyonj@hotmail.com>
2020-09-22 09:24:18 -0400

    add routesand linkURL

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,4 @@
 class ApplicationController < ActionController::API
     include ::ActionController::Cookies
     
-    def current_user
-      User.find_by(id: session[:user_id])
-    end
-  
-    def signed_in?
-      !!current_user
-    end
   end
--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ApplicationRecord
-    has_secure_password
-    has_many :categories
-    has_many :items, through: :categories
-
-    validates :email, uniqueness: true, presence: true
-    validates :password_digest, presence: true
-  end
